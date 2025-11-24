ca984fc04ad0a80531bd34ebd5468334bcd96970
BerkeWest <berkebati@gmail.com>
2025-11-24 13:48:26 +0300

    Top movies changed to Pager

--- app/src/main/java/com/example/absolutecinema/presentation/home/HomeScreen.kt
+++ app/src/main/java/com/example/absolutecinema/presentation/home/HomeScreen.kt
@@ -6,9 +6,13 @@ import androidx.compose.animation.AnimatedVisibility
 import androidx.compose.animation.core.animateFloatAsState
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.interaction.MutableInteractionSource
+import androidx.compose.foundation.interaction.collectIsDraggedAsState
+import androidx.compose.foundation.interaction.collectIsPressedAsState
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
@@ -19,6 +23,9 @@ import androidx.compose.foundation.lazy.grid.GridCells
 import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
 import androidx.compose.foundation.lazy.grid.items
 import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.pager.HorizontalPager
+import androidx.compose.foundation.pager.PageSize
+import androidx.compose.foundation.pager.rememberPagerState
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.verticalScroll
@@ -50,6 +57,7 @@ import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import androidx.compose.ui.util.lerp
 import androidx.hilt.lifecycle.viewmodel.compose.hiltViewModel
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import coil.compose.AsyncImage
@@ -58,6 +66,7 @@ import com.example.absolutecinema.BuildConfig
 import com.example.absolutecinema.R
 import com.example.absolutecinema.domain.model.response.MovieSearchResultDomainModel
 import kotlinx.coroutines.delay
+import kotlin.math.absoluteValue
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
@@ -81,7 +90,7 @@ fun HomeScreen(
 
             Spacer(modifier = Modifier.height(12.dp))
 
-            TopMoviesCarousel(uiState.topMovies, onNavigateToDetails)
+            TopMoviesPager(uiState.topMovies, onNavigateToDetails)
 
             Spacer(modifier = Modifier.height(16.dp))
 
@@ -94,6 +103,110 @@ fun HomeScreen(
                 tab3 = uiState.topRated,
                 tab4 = uiState.popular,
                 onNavigateToDetails = onNavigateToDetails
+            )
+        }
+    }
+}
+
+@Composable
+fun TopMoviesPager(
+    itemList: List<MovieSearchResultDomainModel>,
+    onNavigateToDetails: (Int) -> Unit
+) {
+    if (itemList.isEmpty()) return
+
+    val initialPage = remember(itemList.size) {
+        val middle = Int.MAX_VALUE / 2
+        // Adjust to point to the first item in the list
+        middle - (middle % itemList.size)
+    }
+
+    // 2. SET PAGE COUNT TO MAX_VALUE
+    val pagerState = rememberPagerState(
+        initialPage = initialPage,
+        pageCount = { Int.MAX_VALUE }
+    )
+
+    val pagerIsDragged by pagerState.interactionSource.collectIsDraggedAsState()
+
+    val pageInteractionSource = remember { MutableInteractionSource() }
+    val pageIsPressed by pageInteractionSource.collectIsPressedAsState()
+
+    val autoAdvance = !pagerIsDragged && !pageIsPressed
+
+    if (autoAdvance) {
+        LaunchedEffect(pagerState, pageInteractionSource) {
+            while (true) {
+                delay(5000)
+                pagerState.animateScrollToPage(pagerState.currentPage + 1)
+            }
+        }
+    }
+
+    HorizontalPager(
+        state = pagerState,
+        pageSize = PageSize.Fixed(220.dp),
+        contentPadding = PaddingValues(horizontal = 20.dp),
+        pageSpacing = 6.dp
+    ) { page ->
+        val actualIndex = page % itemList.size
+        val movie = itemList[actualIndex]
+
+        val pageOffset = (
+                (pagerState.currentPage - page) + pagerState
+                    .currentPageOffsetFraction
+                ).absoluteValue
+
+        val scale = lerp(
+            start = 0.85f,
+            stop = 1f,
+            fraction = 1f - pageOffset.coerceIn(0f, 1f)
+        )
+
+        Box(
+            modifier = Modifier
+                .width(180.dp)
+                .height(260.dp)
+                .graphicsLayer {
+                    scaleX = scale
+                    scaleY = scale
+
+                    alpha = lerp(
+                        start = 0.5f,
+                        stop = 1f,
+                        fraction = 1f - pageOffset.coerceIn(0f, 1f)
+                    )
+                }
+                .clickable { movie.id?.let(onNavigateToDetails) }
+        ) {
+            AsyncImage(
+                model = ImageRequest.Builder(LocalContext.current)
+                    .data(BuildConfig.IMAGE_URL + movie.posterPath)
+                    .crossfade(true)
+                    .build(),
+                error = painterResource(R.drawable.ic_broken_image),
+                placeholder = painterResource(R.drawable.loading_img),
+                contentDescription = movie.title,
+                contentScale = ContentScale.Crop,
+                modifier = Modifier
+                    .fillMaxSize(0.9f)
+                    .align(Alignment.TopEnd)
+                    .clip(RoundedCornerShape(12.dp))
+            )
+
+            Text(
+                text = (actualIndex + 1).toString(),
+                fontSize = (60 * scale).sp,
+                fontWeight = FontWeight.Bold,
+                color = Color(0xFFACDFFA),
+                style = TextStyle(
+                    shadow = Shadow(
+                        color = Color(0xFF0296E5), // Outline color (cyan)
+                        offset = Offset(0f, 0f),
+                        blurRadius = 20f // controls how thick the outline looks
+                    )
+                ),
+                modifier = Modifier.align(Alignment.BottomStart)
             )
         }
     }
@@ -310,6 +423,137 @@ private fun HomeTabs(
 }
 
 //region Previews
+@Preview
+@Composable
+fun TopMoviesPagerPreview() {
+    val itemList = listOf(
+        MovieSearchResultDomainModel(
+            genreIds = listOf(1, 2, 3),
+            id = 2,
+            originalTitle = "Demon Slayer",
+            overview = "Overview",
+            popularity = 1.0,
+            posterPath = "posterPath",
+            releaseDate = "2",
+            title = "Demon Slayer",
+            voteAverage = 4.8,
+            genre = "Anime, Action, Animation, Drama, Fantasy"
+        ),
+        MovieSearchResultDomainModel(
+            genreIds = listOf(1, 2, 3),
+            id = 1,
+            originalTitle = "Demon Slayer",
+            overview = "Overview",
+            popularity = 1.0,
+            posterPath = "posterPath",
+            releaseDate = "2",
+            title = "Demon Slayer",
+            voteAverage = 4.8,
+            genre = "Anime, Action, Animation, Drama, Fantasy"
+        ),
+        MovieSearchResultDomainModel(
+            genreIds = listOf(1, 2, 3),
+            id = 3,
+            originalTitle = "Demon Slayer",
+            overview = "Overview",
+            popularity = 1.0,
+            posterPath = "posterPath",
+            releaseDate = "2",
+            title = "Demon Slayer",
+            voteAverage = 4.8,
+            genre = "Anime, Action, Animation, Drama, Fantasy"
+        ),
+        MovieSearchResultDomainModel(
+            genreIds = listOf(1, 2, 3),
+            id = 4,
+            originalTitle = "Demon Slayer",
+            overview = "Overview",
+            popularity = 1.0,
+            posterPath = "posterPath",
+            releaseDate = "2",
+            title = "Demon Slayer",
+            voteAverage = 4.8,
+            genre = "Anime, Action, Animation, Drama, Fantasy"
+        ),
+        MovieSearchResultDomainModel(
+            genreIds = listOf(1, 2, 3),
+            id = 5,
+            originalTitle = "Demon Slayer",
+            overview = "Overview",
+            popularity = 1.0,
+            posterPath = "posterPath",
+            releaseDate = "2",
+            title = "Demon Slayer",
+            voteAverage = 4.8,
+            genre = "Anime, Action, Animation, Drama, Fantasy"
+        ),
+        MovieSearchResultDomainModel(
+            genreIds = listOf(1, 2, 3),
+            id = 6,
+            originalTitle = "Demon Slayer",
+            overview = "Overview",
+            popularity = 1.0,
+            posterPath = "posterPath",
+            releaseDate = "2",
+            title = "Demon Slayer",
+            voteAverage = 4.8,
+            genre = "Anime, Action, Animation, Drama, Fantasy"
+        ),
+        MovieSearchResultDomainModel(
+            genreIds = listOf(1, 2, 3),
+            id = 7,
+            originalTitle = "Demon Slayer",
+            overview = "Overview",
+            popularity = 1.0,
+            posterPath = "posterPath",
+            releaseDate = "2",
+            title = "Demon Slayer",
+            voteAverage = 4.8,
+            genre = "Anime, Action, Animation, Drama, Fantasy"
+        ),
+        MovieSearchResultDomainModel(
+            genreIds = listOf(1, 2, 3),
+            id = 8,
+            originalTitle = "Demon Slayer",
+            overview = "Overview",
+            popularity = 1.0,
+            posterPath = "posterPath",
+            releaseDate = "2",
+            title = "Demon Slayer",
+            voteAverage = 4.8,
+            genre = "Anime, Action, Animation, Drama, Fantasy"
+        ),
+        MovieSearchResultDomainModel(
+            genreIds = listOf(1, 2, 3),
+            id = 9,
+            originalTitle = "Demon Slayer",
+            overview = "Overview",
+            popularity = 1.0,
+            posterPath = "posterPath",
+            releaseDate = "2",
+            title = "Demon Slayer",
+            voteAverage = 4.8,
+            genre = "Anime, Action, Animation, Drama, Fantasy"
+        ), MovieSearchResultDomainModel(
+            genreIds = listOf(1, 2, 3),
+            id = 0,
+            originalTitle = "Demon Slayer",
+            overview = "Overview",
+            popularity = 1.0,
+            posterPath = "posterPath",
+            releaseDate = "2",
+            title = "Demon Slayer",
+            voteAverage = 4.8,
+            genre = "Anime, Action, Animation, Drama, Fantasy"
+        )
+    )
+
+    TopMoviesPager(
+        itemList = itemList,
+        onNavigateToDetails = {}
+    )
+}
+
 @Preview
 @Composable
 fun TopMoviesCarouselPreview() {

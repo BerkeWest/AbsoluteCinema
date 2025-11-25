1252799febadd070b28f44d091275bb3bd16e6b4
BerkeWest <berkebati@gmail.com>
2025-11-25 10:00:45 +0300

    PlaceholderText centered

--- app/src/main/java/com/example/absolutecinema/presentation/detail/components/CustomTextUtils.kt
+++ app/src/main/java/com/example/absolutecinema/presentation/detail/components/CustomTextUtils.kt
@@ -2,6 +2,7 @@ package com.example.absolutecinema.presentation.detail.components
 
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
@@ -15,6 +16,7 @@ import androidx.compose.ui.graphics.painter.Painter
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -49,7 +51,8 @@ fun PlaceholderText(
     Text(
         stringResource(resource),
         color = Color.Gray,
-        modifier = Modifier.padding(16.dp)
+        modifier = Modifier.padding(16.dp).fillMaxWidth(),
+        textAlign = TextAlign.Center
     )
 }
 

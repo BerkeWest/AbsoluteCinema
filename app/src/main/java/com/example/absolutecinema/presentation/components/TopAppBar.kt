98347eb7e1292d1729294f51f88e2913e3c7f954
BerkeWest <berkebati@gmail.com>
2025-12-01 17:48:17 +0300

    Toggle for trending movies added

--- app/src/main/java/com/example/absolutecinema/presentation/components/TopAppBar.kt
+++ app/src/main/java/com/example/absolutecinema/presentation/components/TopAppBar.kt
@@ -1,5 +1,16 @@
 package com.example.absolutecinema.presentation.components
 
+import androidx.compose.animation.core.animateDpAsState
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.offset
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.CenterAlignedTopAppBar
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
@@ -11,10 +22,16 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.absolutecinema.R
 
 @OptIn(ExperimentalMaterial3Api::class)
@@ -27,7 +44,9 @@ fun TopAppBar(
     isBookmarked: Boolean,
     bookmark: () -> Unit,
     accountAccess: () -> Boolean,
-    logout: () -> Unit
+    logout: () -> Unit,
+    timeWindow: String? = null,
+    timeWindowAccess: Boolean? = false
 ) {
     CenterAlignedTopAppBar(
         title = { Text(title) },
@@ -58,6 +77,14 @@ fun TopAppBar(
                         contentDescription = stringResource(R.string.logout_button)
                     )
                 }
+            } else if (timeWindowAccess == true) {
+                var selected by remember { mutableStateOf(0) }
+
+                DoubleSwitch(
+                    selectedIndex = selected,
+                    onSelectedChange = { selected = it }
+                )
+
             }
         },
         colors = TopAppBarDefaults.topAppBarColors(
@@ -69,11 +96,61 @@ fun TopAppBar(
     )
 }
 
+@Composable
+fun DoubleSwitch(
+    modifier: Modifier = Modifier,
+    options: List<String> = listOf("Day", "Week"),
+    selectedIndex: Int,
+    onSelectedChange: (Int) -> Unit
+) {
+    val animatedOffset by animateDpAsState(
+        targetValue = (selectedIndex * 50).dp, // width of each segment
+        label = "switchOffset"
+    )
+
+    Box(
+        modifier = modifier
+            .fillMaxWidth(0.3f)
+            .fillMaxHeight(0.5f)
+            .padding(horizontal = 10.dp)
+            .clip(RoundedCornerShape(50))
+            .background(Color(0xFF2A2A2A))
+    ) {
+        Box(
+            modifier = Modifier
+                .offset(x = animatedOffset)
+                .fillMaxWidth(0.5f)
+                .fillMaxHeight()
+                .clip(RoundedCornerShape(50))
+                .background(Color(0xFF2A80FF))
+        )
+        Row(Modifier.fillMaxSize()) {
+            options.forEachIndexed { index, text ->
+                Box(
+                    modifier = Modifier
+                        .weight(1f)
+                        .fillMaxHeight()
+                        .clickable { onSelectedChange(index) },
+                    contentAlignment = Alignment.Center
+                ) {
+                    Text(
+                        text = text,
+                        color = if (selectedIndex == index) Color.Black else Color.White,
+                        fontWeight = FontWeight.Bold,
+                        fontSize = 12.sp
+                    )
+                }
+            }
+        }
+    }
+}
+
+
 //region Previews
 @Preview
 @Composable
-fun DetailTopAppBarPreview(){
-    var bookmarked by remember {mutableStateOf(false)}
+fun DetailTopAppBarPreview() {
+    var bookmarked by remember { mutableStateOf(false) }
     TopAppBar(
         title = stringResource(R.string.details),
         canNavigateBack = true,
@@ -88,7 +165,7 @@ fun DetailTopAppBarPreview(){
 
 @Preview
 @Composable
-fun WatchListTopAppBarPreview(){
+fun WatchListTopAppBarPreview() {
     TopAppBar(
         title = stringResource(R.string.watchlist),
         canNavigateBack = false,
@@ -96,8 +173,19 @@ fun WatchListTopAppBarPreview(){
         canBookmark = false,
         isBookmarked = false,
         bookmark = { },
-        accountAccess = { true },
-        logout = {}
+        accountAccess = { false },
+        logout = {},
+        timeWindow = "Day",
+        timeWindowAccess = true
+    )
+}
+
+@Preview
+@Composable
+fun DoubleSwitchPreview() {
+    DoubleSwitch(
+        selectedIndex = 0,
+        onSelectedChange = {}
     )
 }
 //endregion

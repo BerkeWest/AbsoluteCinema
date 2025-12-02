0b53f4237721a80881bbbb192981d0cfcdc6b979
BerkeWest <berkebati@gmail.com>
2025-12-02 13:33:02 +0300

    colors added

--- app/src/main/java/com/example/absolutecinema/presentation/theme/Color.kt
+++ app/src/main/java/com/example/absolutecinema/presentation/theme/Color.kt
@@ -9,3 +9,15 @@ val Pink80 = Color(0xFFEFB8C8)
 val Purple40 = Color(0xFF6650a4)
 val PurpleGrey40 = Color(0xFF625b71)
 val Pink40 = Color(0xFF7D5260)
+
+val RatingColor = Color(0xFFFF8700)
+
+val FocusedSearchBarColor = Color(0xFF3A3F47)
+
+val UnfocusedSearchBarColor = Color(0xFF67686D)
+
+val SelectedColor = Color(0xFF0296E5)
+
+val BackgroundColor = Color(0xFF242A32)
+
+val RankColor = Color(0xFFACDFFA)

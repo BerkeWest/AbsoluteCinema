0ec08d18a20397ae3ff27e914e06eacef3ef5a93
BerkeWest <berkebati@gmail.com>
2025-12-03 10:41:30 +0300

    Refactor DoubleSwitch

--- app/src/main/java/com/example/absolutecinema/presentation/components/TopAppBar.kt
+++ app/src/main/java/com/example/absolutecinema/presentation/components/TopAppBar.kt
@@ -1,17 +1,5 @@
 package com.example.absolutecinema.presentation.components
 
-import androidx.compose.animation.core.animateDpAsState
-import androidx.compose.foundation.background
-import androidx.compose.foundation.border
-import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.fillMaxHeight
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.offset
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.CenterAlignedTopAppBar
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
@@ -24,16 +12,10 @@ import androidx.compose.runtime.mutableIntStateOf
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
-import androidx.compose.ui.Alignment
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
 import com.example.absolutecinema.R
 
 @OptIn(ExperimentalMaterial3Api::class)
@@ -79,8 +61,7 @@ fun TopAppBar(
                         contentDescription = stringResource(R.string.logout_button)
                     )
                 }
-            } /*
-            else if (timeWindowAccess == true) {
+            } else if (timeWindowAccess == true) {
                 var selected by remember { mutableIntStateOf(if (timeWindowDay) 0 else 1) }
 
                 DoubleSwitch(
@@ -88,7 +69,7 @@ fun TopAppBar(
                     onSelectedChange = { selected = it }
                 )
 
-            }*/
+            }
         },
         colors = TopAppBarDefaults.topAppBarColors(
             containerColor = Color(0xFF242A32),
@@ -98,58 +79,6 @@ fun TopAppBar(
         ),
     )
 }
-
-/*
-@Composable
-fun DoubleSwitch(
-    modifier: Modifier = Modifier,
-    options: List<String> = listOf("Day", "Week"),
-    selectedIndex: Int?,
-    onSelectedChange: (Int) -> Unit
-) {
-    val animatedOffset by animateDpAsState(
-        targetValue = (selectedIndex?.times(50))?.dp ?: 0.dp,
-        label = "switchOffset"
-    )
-
-    Box(
-        modifier = modifier
-            .fillMaxWidth(0.3f)
-            .fillMaxHeight(0.5f)
-            .padding(horizontal = 10.dp)
-            .clip(RoundedCornerShape(50))
-            .background(Color(0xFF2A2A2A))
-            .border(width = 1.dp, color = Color(0xFF2A80FF), shape = RoundedCornerShape(50)),
-    ) {
-        Box(
-            modifier = Modifier
-                .offset(x = animatedOffset)
-                .fillMaxWidth(0.5f)
-                .fillMaxHeight()
-                .clip(RoundedCornerShape(50))
-                .background(Color(0xFF2A80FF))
-        )
-        Row(Modifier.fillMaxSize()) {
-            options.forEachIndexed { index, text ->
-                Box(
-                    modifier = Modifier
-                        .weight(1f)
-                        .fillMaxHeight()
-                        .clickable { onSelectedChange },
-                    contentAlignment = Alignment.Center
-                ) {
-                    Text(
-                        text = text,
-                        color = if (selectedIndex == index) Color.Black else Color.White,
-                        fontWeight = FontWeight.Bold,
-                        fontSize = 12.sp
-                    )
-                }
-            }
-        }
-    }
-}
-*/
 
 //region Previews
 @Preview
